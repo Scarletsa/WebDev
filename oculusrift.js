3800b250dcc45ca872aecaf32344e3ab5373949b
Michael Albrecht <m.ryan.albrecht@hotmail.com>
2016-12-06 10:27:21 -0600

    Jquery example
    Got oculusrift.js working using jquery. Much more elegent and easy to
    follow.

--- oculusrift.js
+++ oculusrift.js
@@ -1,6 +1,6 @@
 var quantity = 0;
 var oculus = [];
-function start() {
+/*function start() {
   //$("#wishlist").click(wish())
   var button1 = document.getElementById("wishlist");
   button1.addEventListener("click", wish, false);
@@ -23,8 +23,27 @@ function wish() {
 
 function cart() {
   var price = 799;
-  var quantity = /* retrieve integer from cart page */
+  var quantity = //retrieve integer from cart page
   alert("Oculus Rift added to cart");
 }
 window.addEventListener("load", start, false);
-//$(document).ready(start());
+//$(document).ready(start()); */
+
+$(document).ready(function(){
+      $("#wishlist").click(function() {
+        var price = 799;
+        var description = "Oculus Rift VR";
+        quantity ++;
+        var total = (price * quantity);
+        var item = (description + "|" + price + "|" + quantity + "|" + total);
+        oculus.push(item);
+        window.localStorage.setItem("wishListArray", (oculus.join(";")));
+        alert("Oculus Rift added to wish list");
+      })
+
+      $("#shoppingcart").click(function() {
+        var price = 799;
+        var quantity = /* retrieve integer from cart page */
+        alert("Oculus Rift added to cart");
+      });
+});
